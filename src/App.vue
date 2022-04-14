866c2bd09f7f10e7f03f0bd5f301960f53d17f5b
iOS-Bothofus <30752890+Surajmendy@users.noreply.github.com>
2022-04-14 09:22:44 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <GoogleMapComponent/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMapComponent from './components/GoogleMapComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GoogleMapComponent
   }
 }
 </script>
@@ -22,5 +23,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.vue-map-container {
+  z-index: -1 !important;
+  width: 100% !important; 
+  height: 100% !important;
 }
 </style>
